4957dcd4f3f9bddb2a2276cbb20bb9154eba2cfe
Harsh <harshug921@gmail.com>
2024-02-16 15:31:25 +0530

    coomit

--- src/components/UserSearch.js
+++ src/components/UserSearch.js
@@ -2,19 +2,22 @@
 
 import React, { useState } from 'react';
 import PlayerProfile from './PlayerProfile';
+import axios from 'axios';
 
 const UserSearch = ({ onPlayerSelect }) => {
   const [username, setUsername] = useState('');
   const [searchedPlayer, setSearchedPlayer] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [data, setData] = useState()
 
-  const handleSearch = async () => {
+
+  /*const handleSearch = async () => {
     setLoading(true);
 
     try {
       // Perform a search for the specified username from Lichess API
-      const response = await fetch(`https://lichess.org/api/user/${username}`);
-      const data = await response.json();
+      const response = await fetch(`https://lichess.org/api/user/${username}`, {mode:'cors'});
+      console.log(response)
 
       // Update the searchedPlayer state with fetched data
       const updatedSearchedPlayer = {
@@ -22,7 +25,7 @@ const UserSearch = ({ onPlayerSelect }) => {
         username: data.username,
         avatar: data.avatar,
         followers: data.follows.length,
-        gamesPlayed: data.pergames?.total?.games
+        gamesPlayed: data.pergames?.total?.games,
       };
 
       // Call onPlayerSelect with the selected player data
@@ -37,6 +40,17 @@ const UserSearch = ({ onPlayerSelect }) => {
     }
   };
 
+
+  */
+  const handleSearch = async () => {
+    setLoading(true);
+    await axios.get(`https://lichess.org/api/user/${username}`).then((res) => {
+      setData(res.data);
+      console.log(res.data)
+    })
+     
+  }
+  
   return (
     <div>
       <h3>User Search</h3>
@@ -50,13 +64,20 @@ const UserSearch = ({ onPlayerSelect }) => {
         {loading ? 'Searching...' : 'Search'}
       </button>
 
-      {searchedPlayer && (
+      {data && (
         <>
           <h4>Search Results</h4>
           {/* Display the searched player's profile */}
-          <PlayerProfile player={searchedPlayer} />
+          {/* <PlayerProfile player={searchedPlayer} /> */}
+      
+          <h2>{data.username}</h2> {/* Update this line */}
+          <h2>Classical rating :{data.perfs.classical.rating}</h2> {/* Update this line */}
+          <p>Total Games: {data.count.all}</p> {/* Update this line */}
+          <p>Created at: {data.playTime.total}</p> {/* Update this line */}
+    
         </>
       )}
+
     </div>
   );
 };
